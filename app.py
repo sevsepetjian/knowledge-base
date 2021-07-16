f47ea862a5421c729510be286b0de7cd93bb5051
Sevag Sepetjian <sevsepetjian@pm.me>
2021-07-15 21:55:12 -0700

    Working on DS algorithim for dir folders.

--- app.py
+++ app.py
@@ -3,18 +3,48 @@ import markdown
 import os
 
 app = Flask(__name__)
-root_dir = os.path.dirname(os.path.abspath(__file__))
-for subdir, dirs, files in os.walk(root_dir + '/categories'):
-    for sub in dirs:
-        print(sub)
-    for file in files:
-        print(file)
-# sub_dir = os.listdir(root_dir + '/categories')
-# for file in sub_dir:
-#     print(os.listdir(root_dir + '/categories/' + file))
+
+root_dir = os.path.dirname(os.path.abspath(__file__)) + '/categories'
+# https://stackoverflow.com/questions/41113929/generate-an-html-directory-tree-in-python
+def generate_tree(path, folders = {}): 
+    for file in os.listdir(path):
+        rel = path + '/' + file
+        if os.path.isdir(rel):
+            print(file)
+            generate_tree(rel)
+        else:
+            print(file)
+    return folders
+
+print(generate_tree(root_dir))
+# dict = {}
+# dict['first_name'] = 'Sevag'
+# dict['last_name'] = 'Sepetjian'
+# dict['arr'] = []
+# dict['arr'].append('Appeneded')
+# print(dict)
+
+# dict = {
+#     'computer-science': 'cs.md',
+#     'philosophy': [
+#         { 'jungian': ['jungian.md'] },
+#         'philosophy.md'
+#     ]
+# }
+
+# dict['computer-science'] = 'python.md'
+
+# print(dict)
+# for items in dict:
+#     print(items)
+
+# for path, dirs, files in os.walk(root_dir):
+#     for dir in dirs:
+#         for file in os.listdir(path + '/' + dir):
+#             print(file)
 @app.route('/')
 def home_page():
-    with open(root_dir + './README.md', 'r') as input_file:
+    with open(root_dir + './computer-science/cs.md', 'r') as input_file:
         text = input_file.read()
     html = markdown.markdown(text)
     
