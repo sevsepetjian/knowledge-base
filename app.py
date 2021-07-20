61e2a39276b5b982f363dbdb3a6cf2569abf6e01
Sevag Sepetjian <sevsepetjian@pm.me>
2021-07-19 21:26:57 -0700

    Progress on tree ds algorithim.

--- app.py
+++ app.py
@@ -6,42 +6,24 @@ app = Flask(__name__)
 
 root_dir = os.path.dirname(os.path.abspath(__file__)) + '/categories'
 # https://stackoverflow.com/questions/41113929/generate-an-html-directory-tree-in-python
-def generate_tree(path, folders = {}): 
+def generate_tree(path, folders = {}, current_dir = ''): 
     for file in os.listdir(path):
         rel = path + '/' + file
         if os.path.isdir(rel):
-            print(file)
-            generate_tree(rel)
+            current_dir = file
+            folders[file] = []
+            generate_tree(rel, folders, current_dir)
+        # elif os.path.isdir(path + '/' + current_dir) and current_dir != '':
+        #     print(current_dir)
+        #     current_dir = file
+        #     generate_tree(rel, folders, current_dir)
         else:
-            print(file)
+            folders[current_dir].append(file)
+            current_dir = file
     return folders
 
-print(generate_tree(root_dir))
-# dict = {}
-# dict['first_name'] = 'Sevag'
-# dict['last_name'] = 'Sepetjian'
-# dict['arr'] = []
-# dict['arr'].append('Appeneded')
-# print(dict)
+print(generate_tree(root_dir, {}, ''))
 
-# dict = {
-#     'computer-science': 'cs.md',
-#     'philosophy': [
-#         { 'jungian': ['jungian.md'] },
-#         'philosophy.md'
-#     ]
-# }
-
-# dict['computer-science'] = 'python.md'
-
-# print(dict)
-# for items in dict:
-#     print(items)
-
-# for path, dirs, files in os.walk(root_dir):
-#     for dir in dirs:
-#         for file in os.listdir(path + '/' + dir):
-#             print(file)
 @app.route('/')
 def home_page():
     with open(root_dir + './computer-science/cs.md', 'r') as input_file:
