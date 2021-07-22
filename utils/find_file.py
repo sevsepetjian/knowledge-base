6f8d87efd11d9f43fc9db781cd73756c657fefc4
Sevag Sepetjian <sevsepetjian+dev@pm.me>
2021-07-21 18:09:38 -0700

    Edited comments.

--- utils/find_file.py
+++ utils/find_file.py
@@ -4,7 +4,7 @@ def find_file(path = '', file_name = ''):
     for root, dirs, files in os.walk(path):
         # select file name
         for file in files:
-            # check the extension of files
+            # check file name
             if file == file_name:
                 # print whole path of files
                 return os.path.join(root, file)
