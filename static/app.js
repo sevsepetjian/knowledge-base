0b8bf67ca367816b32b2ea3f73230b642ff52979
Sevag Sepetjian <sevsepetjian+dev@pm.me>
2021-07-22 15:39:35 -0700

    [1] Created mobile menu sidebar and navbar.

--- static/app.js
+++ static/app.js
@@ -2,4 +2,30 @@ console.log('Client JS loaded.')
 
 const navbarBurger = document.querySelector('.navbar-burger')
 const menu = document.querySelector('.menu')
-navbarBurger.addEventListener('click', () => menu.classList.toggle('is-hidden-mobile'))
+const body = document.body
+
+const createMenuOverlay = document.createElement('div')
+createMenuOverlay.classList.add('menu-overlay')
+
+const state = {
+    menuOpen: false
+}
+
+const toggleMenu = () => {
+    // Set the state to the opposite of the current value
+    state.menuOpen = !state.menuOpen
+
+    if (state.menuOpen) {
+       body.appendChild(createMenuOverlay) 
+    } else {
+        const menuOverlay = document.querySelector('.menu-overlay')
+        menuOverlay.remove()    
+    }
+
+    menu.classList.toggle('is-hidden-mobile')
+    menu.style.transform = 'translateX(0)'
+}
+
+navbarBurger.addEventListener('click', () => toggleMenu())
+
+createMenuOverlay.addEventListener('click', () => toggleMenu())
