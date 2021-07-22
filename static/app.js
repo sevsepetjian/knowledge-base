ec0eea3ac9285091a13a9cf58718b5d40387c687
Sevag Sepetjian <sevsepetjian+dev@pm.me>
2021-07-22 12:45:02 -0700

    [1] Edited and created more py comments. [2] Figured out jinja templating. [3] Began working on navbar and sidebar.

--- static/app.js
+++ static/app.js
@@ -1,1 +1,5 @@
 console.log('Client JS loaded.')
+
+const navbarBurger = document.querySelector('.navbar-burger')
+const menu = document.querySelector('.menu')
+navbarBurger.addEventListener('click', () => menu.classList.toggle('is-hidden-mobile'))
--- utils/generate_tree_html.py
+++ utils/generate_tree_html.py
@@ -2,18 +2,15 @@ import os
 from pathlib import Path
 
 def generate_tree_html(path, html = ''):
-   # path = Path(__file__).parent.parent
-   # full_path = str(path) + '/categories'
-
     for file in os.listdir(path):
         # go through each subdir and file in folder. If its dir recursive call, else its .md continue
         rel = path + '/' + file
         file_no_md = file.replace('.md', ' ')
         file_title_name = str.title(file_no_md.replace('-', ' '))
         if os.path.isdir(rel):
-            html += "<div class='child'><h1>%s</h1>" % (file_title_name)
+            html += "<p class='menu-label'>%s</p>" % (file_title_name)
             html += generate_tree_html(rel)
-            html += "</ul></div>"
+            html += "</ul>"
         else:
-            html += "<ul><li><a href='/article/%s'>%s</a></li>" % (file_no_md, file_title_name)
+            html += "<ul class='menu-list'><li><a href='/article/%s'>%s</a></li>" % (file_no_md, file_title_name)
     return html
